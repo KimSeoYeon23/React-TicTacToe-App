de7b772be8ca05638a717b8f26e06467ba81cae7
SeoYeon <sollucidus0221@naver.com>
2023-03-22 20:23:20 +0900

    state 추가

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 import './Square.css'
 
 export default class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     )
   }
