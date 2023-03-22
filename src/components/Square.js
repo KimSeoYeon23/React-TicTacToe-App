9956c71b3d0e7b03d245a49964e6d100113b4593
SeoYeon <sollucidus0221@naver.com>
2023-03-22 22:51:25 +0900

    Square class

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,18 @@
-import React from "react";
-import "./Square.css";
+import React, { Component } from "react";
+import './Square.css'
 
-const Square = ({ onClick, value }) => {
+export default class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+  render() {
     return (
-        <button className="square"
-            onClick={onClick}>
-            {value}
-        </button>
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
     )
+  }
 }
-
-export default Square;
