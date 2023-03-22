75bb16543dd123b56f81a6848b9eba9405827fcb
SeoYeon <sollucidus0221@naver.com>
2023-03-22 22:24:37 +0900

    순서 만들기

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,23 +3,26 @@ import Square from './Square'
 import './Board.css'
 
 const Board = () => {
-
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+    newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
+    setXIsNext(current => !current);
   }
 
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />
+    return (
+      <Square value={squares[i]} onClick={() => handleClick(i)} />
+    )
   }
+
   return (
     <div>
-      <div className='status'>
-        Next Player: X, O
-      </div>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
@@ -39,4 +42,4 @@ const Board = () => {
   )
 }
 
-export default Board();
+export default Board;
